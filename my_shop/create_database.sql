29f729dcc44c502d7275344cf563ae0073da8f13
Yohei Yoshikawa <yoo@MBP.local>
2020-12-02 22:04:30 +0900

    UC

--- my_shop/create_database.sql
+++ my_shop/create_database.sql
@@ -1,53 +1,33 @@
 CREATE TABLE users (
-    id bigint UNSIGNED NOT NULL,
+    id bigint UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
     email_verified_at timestamp NULL DEFAULT NULL,
     password varchar(255) NOT NULL,
     remember_token varchar(100) DEFAULT NULL,
-    created_at timestamp NULL DEFAULT NULL,
-    updated_at timestamp NULL DEFAULT NULL
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE users
-    ADD PRIMARY KEY (id),
-    ADD UNIQUE KEY users_email_unique (email);
-
-ALTER TABLE users
-    MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
 CREATE TABLE items (
-    id bigint UNSIGNED NOT NULL,
-    code varchar(255) NOT NULL,
+    id bigint UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    code varchar(255) UNIQUE NOT NULL,
     name varchar(255) NOT NULL,
     price int NOT NULL,
     amount int NOT NULL,
-    created_at timestamp NULL DEFAULT NULL,
-    updated_at timestamp NULL DEFAULT NULL
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE items 
-    ADD PRIMARY KEY (id),
-    ADD UNIQUE KEY items_code_unique (code);
-
-ALTER TABLE items 
-    MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE user_items (
-    id bigint UNSIGNED NOT NULL,
+    id bigint UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id bigint UNSIGNED NOT NULL,
     item_id bigint UNSIGNED NOT NULL,
-    price int NOT NULL,
     amount int NOT NULL,
     total_price int NOT NULL,
-    created_at timestamp NULL DEFAULT NULL,
-    updated_at timestamp NULL DEFAULT NULL
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE user_items ADD PRIMARY KEY (id);
-
-ALTER TABLE user_items
-  MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE user_items
   ADD CONSTRAINT user_items_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
