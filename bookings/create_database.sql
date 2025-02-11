c815b2084b8a1529f427bc419383cc8333f80500
yoo16 <yohei.yoshikawa@gmail.com>
2025-02-11 15:18:52 +0900

    update

---
+++ bookings/create_database.sql
@@ -0,0 +1,63 @@
+DROP TABLE IF EXISTS user_items;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE users (
+    id bigint UNSIGNED NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    email_verified_at datetime NULL DEFAULT NULL,
+    password varchar(255) NOT NULL,
+    remember_token varchar(100) DEFAULT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NULL DEFAULT NULL
+);
+
+ALTER TABLE users
+    ADD PRIMARY KEY (id),
+    ADD UNIQUE KEY users_email_unique (email);
+
+ALTER TABLE users
+    MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+
+CREATE TABLE items (
+    id bigint UNSIGNED NOT NULL,
+    code varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    price int NOT NULL,
+    amount int NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NULL DEFAULT NULL
+);
+
+ALTER TABLE items 
+    ADD PRIMARY KEY (id),
+    ADD UNIQUE KEY items_code_unique (code);
+
+ALTER TABLE items 
+    MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE user_items (
+    id bigint UNSIGNED NOT NULL,
+    user_id bigint UNSIGNED NOT NULL,
+    item_id bigint UNSIGNED NOT NULL,
+    amount int NOT NULL,
+    total_price int NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime NULL DEFAULT NULL
+);
+
+ALTER TABLE user_items ADD PRIMARY KEY (id);
+
+ALTER TABLE user_items MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE users MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE user_items
+  MODIFY id bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE user_items
+  ADD CONSTRAINT user_items_item_id_fkey FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT user_items_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
